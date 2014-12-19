b5d2466c01533a82e308ecc8985108a068b813f3
Christopher Thornton <rmdirbin@gmail.com>
2014-12-19 10:02:28 -0800

    Added sbin to path

--- zshrc.d/path.zsh
+++ zshrc.d/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
 export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
 export PATH=~/bin:$PATH
