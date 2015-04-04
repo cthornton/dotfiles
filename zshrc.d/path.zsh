09f74e7b4a6046f37466bdb5a3d116696f7d0b5e
Christopher Thornton <rmdirbin@gmail.com>
2015-04-04 10:38:49 -0700

    Add psql 9.4 support to path

--- zshrc.d/path.zsh
+++ zshrc.d/path.zsh
@@ -1,4 +1,7 @@
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
+
+# Note support for both Postgres.app 9.3 and 9.4
 export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH"
 export PATH=/Applications/Sublime\ Text.app/Contents/SharedSupport/bin:$PATH
 export PATH=~/bin:$PATH
