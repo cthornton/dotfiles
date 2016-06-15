0a2ec9bd25093fbb7ef14cfffd93665ffddb7577
Christopher Thornton <cthornton@squareup.com>
2016-06-15 09:27:01 -0700

    Support both OSX and linux for nvm

--- zshrc.d/nvm.zsh
+++ zshrc.d/nvm.zsh
@@ -1,4 +1,7 @@
-if [[ -s "$HOME/.nvm" ]] then
-  export NVM_DIR=~/.nvm
+if command -v brew &> /dev/null && [[ -s `brew --prefix nvm` ]] then
+  source $(brew --prefix nvm)/nvm.sh
+elif [[ -s "$HOME/.nvm/nvm.sh" ]] then
   source "$NVM_DIR/nvm.sh"
 fi
+
+export NVM_DIR=~/.nvm
