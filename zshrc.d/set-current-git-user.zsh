047c72a8a04cc4d96ef4451bfa397556ad17c02d
Christopher Thornton <cthornton@squareup.com>
2021-01-26 14:12:52 -0800

    Add gpg signature support

--- zshrc.d/set-current-git-user.zsh
+++ zshrc.d/set-current-git-user.zsh
@@ -1,15 +1,18 @@
 current_user_path=~/dotfiles/git/current-user.gitconfig
-echo "[user]" > $current_user_path
 
 hostname=`hostname`
 case "$hostname" in
   "dell-laptop.local")
+    echo "[user]" > $current_user_path
     echo "  email=\"cthornton@squareup.com\"" >> $current_user_path
-    echo "  signingkey = AD19B836" >> $current_user_path
+    echo "  signingkey = 7F670F76BB8E4BE2" >> $current_user_path
     echo "[url \"https://github.com/\"]" >> $current_user_path
 	  echo "  insteadOf = git://github.com/" >> $current_user_path
+    echo "[commit]" >> $current_user_path
+    echo "  gpgsign = true" >> $current_user_path
     ;;
   *)
+    echo "[user]" > $current_user_path
     echo "  email=\"rmdirbin@gmail.com\"" >> $current_user_path
     ;;
 esac
