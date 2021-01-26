4b0fcb903b48ce0e66b94717613d8987e4ca9203
Christopher Thornton <cthornton@squareup.com>
2021-01-25 17:54:48 -0800

    Add work config, alias to open PRs from console

--- zshrc.d/set-current-git-user.zsh
+++ zshrc.d/set-current-git-user.zsh
@@ -6,6 +6,8 @@ case "$hostname" in
   "dell-laptop.local")
     echo "  email=\"cthornton@squareup.com\"" >> $current_user_path
     echo "  signingkey = AD19B836" >> $current_user_path
+    echo "[url \"https://github.com/\"]" >> $current_user_path
+	  echo "  insteadOf = git://github.com/" >> $current_user_path
     ;;
   *)
     echo "  email=\"rmdirbin@gmail.com\"" >> $current_user_path
